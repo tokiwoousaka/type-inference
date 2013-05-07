7fce15b32d2f8266c6ce313d277a0e73da61942b
Tokiwo-ousaka <its.out.of.tune.this.my.music@gmail.com>
2013-05-08 02:06:39 +0900

    ちょっと改善

--- Main.hs
+++ Main.hs
@@ -15,14 +15,18 @@ typeName :: Parser TypeTree
 typeName = TypeName <$> word ((:) <$> upper <*> many letter)
 
 typeValue :: Parser TypeTree
-typeValue = TypeValue <$> word (many letter)
+typeValue = TypeValue <$> word ((:) <$> letter <*> many letter)
+
+subTree :: Parser TypeTree
+subTree = SubTree <$> word (parens typeSyntax)
 
 typeSyntax :: Parser [TypeTree]
-typeSyntax = sepBy (SubTree <$> parens typeSyntax <|> typeName <|> typeValue) (symbol "->")
+typeSyntax = sepBy (subTree <|> typeName <|> typeValue) (symbol "->")
 
 main :: IO ()
 main = do
    parseTest typeSyntax "a -> b -> a"
    parseTest typeSyntax "Hoge -> piyo -> Huga"
    parseTest typeSyntax "(a -> b) -> (b -> c) -> a -> c"
-
+   parseTest typeSyntax "(a -> ) -> (c -> d) "
+   parseTest typeSyntax "a ->  -> c"
